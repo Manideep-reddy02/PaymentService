31418c6768def0dad69e79588b426cfbea5514f3
manideep-reddy-02 <gaddammani01@gmail.com>
2024-12-12 16:32:24 +0530

    Integrated stripe

---
+++ src/main/java/com/example/paymentservice/Controllers/StripeWebhook.java
@@ -0,0 +1,10 @@
+package com.example.paymentservice.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stripe/webhooks")
+public class StripeWebhooks {
+    
+}
